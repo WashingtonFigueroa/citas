2417e49943cb0a667bbdd73c6442e18231af0489
WashingtonFigueroa <w.figo.1991@gmail.com>
2021-07-01 22:01:21 -0500

    citas

---
+++ config-sample.php
@@ -0,0 +1,33 @@
+<?php
+class Config {
+
+    // ------------------------------------------------------------------------
+    // GENERAL SETTINGS
+    // ------------------------------------------------------------------------
+
+    const BASE_URL      = 'http://url-to-easyappointments-directory';
+    const LANGUAGE      = 'english';
+    const DEBUG_MODE    = FALSE;
+
+    // ------------------------------------------------------------------------
+    // DATABASE SETTINGS
+    // ------------------------------------------------------------------------
+
+    const DB_HOST       = 'localhost';
+    const DB_NAME       = 'easyappointments';
+    const DB_USERNAME   = 'root';
+    const DB_PASSWORD   = 'root';
+
+    // ------------------------------------------------------------------------
+    // GOOGLE CALENDAR SYNC
+    // ------------------------------------------------------------------------
+
+    const GOOGLE_SYNC_FEATURE   = FALSE; // Enter TRUE or FALSE
+    const GOOGLE_PRODUCT_NAME   = '';
+    const GOOGLE_CLIENT_ID      = '';
+    const GOOGLE_CLIENT_SECRET  = '';
+    const GOOGLE_API_KEY        = '';
+}
+
+/* End of file config.php */
+/* Location: ./config.php */
